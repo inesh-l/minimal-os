986cd644541cdcc91f91bf790a51110bdbc46817
Inesh Loka <ineshrloka@gmail.com>
2024-02-09 22:51:41 -0500

    fix main function

--- src/base/kernel.c
+++ src/base/kernel.c
@@ -1,9 +1,9 @@
-extern void main(){
+extern int main(){
     const char *s = "Hello World";
     char * base = (char *)0xb8000;
     while (*s) {
         *base++ = *s++;
         *base++ = 0x0f;
     }
-    return;
+    return 1;
 }
